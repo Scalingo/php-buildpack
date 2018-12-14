4b119056ffcc87def43966f90d028d2cb395c82a
Soulou <leo@unbekandt.eu>
2018-12-14 17:15:01 +0100

    To fix XFS tail -f error, install custom tail
    This is only true for stack 'scalingo' based on Ubuntu 14.04
    where tail doesn't have XFS support

--- bin/common.sh
+++ bin/common.sh
@@ -64,7 +64,7 @@ init_log_plex() {
 
 tail_log_plex() {
   for log_file in $*; do
-    echo "tail -n 0 -qF --pid=\$\$ ${log_file} 2>&1 | grep -v 'unrecognized file system type' &"
+    echo "tail -n 0 -qF --pid=\$\$ ${log_file} 2>&1 &"
   done
 }
 
