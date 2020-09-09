b1d3adba06eedf95deabd68e67b0ae763244a914
Jonathan Hurter <john@scalingo.com>
2020-09-09 20:08:16 +0200

    Revert "Use tail -f instead of cat"
    This reverts commit 015cd69ef804f611eb54f4bc0e3134a849dcf494.

--- bin/common.sh
+++ bin/common.sh
@@ -46,7 +46,7 @@ init_stdout() {
   done
   for log_file in $*; do
     echo "mkfifo ${log_file}"
-    echo "tail -n 0 -qF --pid=\$\$ ${log_file} 2>&1 &"
+    echo "while true ; do cat ${log_file}; done &"
   done
 }
 
