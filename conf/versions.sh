ca7ae8d8baaa3459c097797efb541dffe8fa03a2
Étienne Michon <etienne@scalingo.com>
2026-05-26 16:30:01 +0200

    feat(versions): downgrade MongoDB extension from 2.0.0 to 1.21.0

--- conf/versions.sh
+++ conf/versions.sh
@@ -42,7 +42,7 @@ zlib_version="${zlib_version:-1.3.2}"
 
 # PHP extensions versions
 igbinary_version="3.2.16"
-mongodb_version="2.0.0" # this is the latest version with support for MongoDB 4.0.x we offer at Scalingo. Hence we need to stick to this version for now.
+mongodb_version="1.21.0"
 amqp_version="2.2.0"
 phpredis_version="6.3.0"
 apcu_version="5.1.28"
